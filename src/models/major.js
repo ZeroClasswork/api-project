1078ce5bf6f3bf4a1bb7179c165155a72934b969
atleastzero <megan.obryan52@gmail.com>
2021-03-03 17:11:51 -0800

    Added major to school dependency in model

--- src/models/major.js
+++ src/models/major.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const MajorSchema = new Schema({
+  school: { type: Schema.Types.ObjectId, ref: "School", required: true },
   type: { type: String, required: true },
   name: { type: String, required: true },
   courses_required: [{ type: Schema.Types.ObjectId, ref: "Course" }],
